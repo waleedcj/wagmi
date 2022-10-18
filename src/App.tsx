19d51a74c17c15cc643d4c8115f61e6d77640214
Ceejay_Da_dj <uwonttakemealive10@gmail.com>
2022-10-18 18:52:32 +0400

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useContractInfiniteReads } from "wagmi";
 
 function App() {
   return (
